f56cfa44f2b3e070c31648f4df71bc3d0665463e
Felipe Marciales <femarpi@hotmail.com>
2020-06-04 23:15:04 -0500

    [ADD]-[UPD] Bootstrap and SideBrand
    Changes:
    * Added bootstrap as a css support.
    * Added SideBrand component as layout's left side.
    * Updated App.vue with both sides of layout.
    * Added mobile behavior for the initial layout.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Login</router-link> |
-      <router-link to="/home">Home</router-link>
-    </div>
-    <router-view />
+  <div class="row no-gutters main-layout">
+    <aside class="col-5 side-brand d-none d-md-flex">
+      <side-brand></side-brand>
+    </aside>
+    <section class="col-7">
+      <router-view />
+    </section>
   </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+html,
+body {
+  height: 100%;
+}
+
+.main-layout {
+  height: 100%;
+  min-height: 100%;
+
+  .side-brand {
+    flex-direction: column;
+    background-image: url('./assets/imagebkg.png');
+    background-size: auto 100%;
+    background-repeat: no-repeat;
+    background-position: left top;
+  }
+}
+</style>
+<script>
+import SideBrand from '@/components/SideBrand';
+export default {
+  components: { SideBrand }
+};
+</script>
---
+++ src/components/SideBrand.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <img alt="" class="store-logo" src="../assets/Logo.png" v-if="currentRouteName" />
+    <img alt="" class="store-brand" src="../assets/Pizza.png" />
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'SideBrand',
+  computed: {
+    currentRouteName() {
+      return this.$route.name === 'Home';
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+section {
+  display: flex;
+  height: 100%;
+}
+
+.store-logo {
+  position: absolute;
+  top: 5%;
+  left: 20%;
+  width: 15%;
+}
+
+.store-brand {
+  margin: auto;
+  width: 90%;
+}
+</style>
